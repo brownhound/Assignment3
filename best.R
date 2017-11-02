0d1efc0c5b64651a0634dbe532f3b328738dbe5e
Benjamin Gaston <gastonb@mtc.midlandstech.edu>
2017-11-02 13:46:56 -0400

    addition of code

--- best.R
+++ best.R
@@ -2,19 +2,37 @@ best <- function(state, outcome) {
   
   ## Read Outcome Data
   
-  outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
   ## Check that the state and outcome are valid
   ## outcomes considered are attack, failure, and pneumonia
   
-  if(!any(state == outcome$State)){
-    stop("Not a valid state")
+  if(!any(state == data$State)){
+    stop(state, " is not a valid state")
   }
-  else if(!any(outcome == c("heart attack", "heart failure", "pneumonia"))){
-    stop("Not a valid outcome class")
+  else if(!any(outcome %in% c("heart attack", "heart failure", "pneumonia"))){
+    stop(outcome, " is not a valid outcome value")
+  }
+  
+  ## Assign column identifier based on outcome identified
+  ## HA = 11, HF = 17, P = 23
+  
+  columnId <- NULL
+  
+  if(outcome == "heart attack"){
+    columnId <- 11
+  }
+  
+  else if(outcome == "heart failure"){
+    columnId <- 17
+  }
+  
+  else if(outcome == "pneumonia"){
+    columnId <- 23
   }
   
   ## Return hospital name in that state with lowest 30-day 
   ## death rate
   
+  return(data)
 }
